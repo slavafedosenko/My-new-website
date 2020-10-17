accac2df6066330bc25d8d0960a19cc143c80d47
slavafedosenko <73021636+slavafedosenko@users.noreply.github.com>
2020-10-17 14:10:36 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f8ac87341c25f001ec3e881",
+				"5f8ac87341c25f001ec3e883"
+			]
+		},
+		"5f8ac87341c25f001ec3e881": {
+			"id": "5f8ac87341c25f001ec3e881",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f8ac87341c25f001ec3e883": {
+			"id": "5f8ac87341c25f001ec3e883",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
